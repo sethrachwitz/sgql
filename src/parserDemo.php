c4b9e004cb3245f07ca56d3e14cded6f0c3f24c6
Seth Rachwitz <srachwitz@gmail.com>
2016-09-24 20:20:50 -0400

    Add WHERE clause

--- src/parserDemo.php
+++ src/parserDemo.php
@@ -2,7 +2,7 @@
 
 require 'lib/parser/parser.php';
 
-$query = "SELECT `customers`:[`id`,`name`,`orders`:[`id`,`cost`,`shipped`],SUM(`orders`:`cost`) AS totalCost]";
+$query = "SELECT `customers`:[`id`,`name`,`orders`:[`id`,`cost`,`shipped`],SUM(`orders`:`cost`) AS totalCost] WHERE `customers`.`orders`:(shipped == true) AND `customers`:(totalCost > 200)";
 
 echo "Query:\n".$query."\n\n";
 
