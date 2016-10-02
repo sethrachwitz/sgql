15f49db421b0b9e91da4f9d8573c9d1f9dbc9aa0
Seth Rachwitz <srachwitz@gmail.com>
2016-10-01 22:03:06 -0400

    Update demo with ORDER keyword

--- src/parserDemo.php
+++ src/parserDemo.php
@@ -2,7 +2,9 @@
 
 require 'lib/parser/parser.php';
 
-$query = "SELECT `customers`:[`id`,`name`,`orders`:[`id`,`cost`,`shipped`],SUM(`orders`:`cost`) AS totalCost] WHERE `customers`.`orders`:(shipped == true) AND `customers`:(totalCost > 200)";
+$query = "SELECT `customers`:[`id`,`name`,`orders`:[`id`,`cost`,`shipped`],SUM(`orders`:`cost`) AS totalCost]
+            WHERE `customers`.`orders`:(shipped == true) AND `customers`:(totalCost > 200)
+            ORDER `customers` BY totalCost ASC";
 
 echo "Query:\n".$query."\n\n";
 
