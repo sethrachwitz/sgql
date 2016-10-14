70dfd6a55075d0d67213e02630768ee745b6a4e0
Seth Rachwitz <srachwitz@gmail.com>
2016-10-13 20:45:00 -0400

    Abstract database driver class
    This class is the abstract definition of a database driver, which is
    responsible for making queries against the database.  Each database will
    have its own class that extends this, which will facilitate differences
    in the APIs used to connect to each database, but provide only one API
    to the code in this project.

---
+++ src/lib/drivers/driver.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace SGQL\Lib\Drivers;
+
+include_once(dirname(__FILE__).'/../config/database/database.php');
+
+use SGQL\Lib\Config as Config;
+use SGQL\Lib\Query as Query;
+
+abstract class Driver {
+    protected $database;
+    protected $dbName;
+    protected $host;
+    protected $connection;
+
+    function __construct($hosts) {
+        $this->database = new Config\Database($hosts);
+    }
+
+    function __destruct() {
+        $this->close();
+    }
+
+    // Returns true if host changed, false if it didn't
+    public function useDatabase($database) {
+        $this->dbName = $database;
+
+        if (is_null($this->host) || !$this->host->hasDatabase($database)) {
+            $this->host = $this->database->getHost($database);
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function query($query) {
+        $queryStr = $this->queryChecks($query);
+
+        $stmt = $this->connection->prepare($queryStr);
+        $stmt->execute($query->getData());
+
+        return $stmt;
+    }
+
+    public function fetchAll($query) {
+        $result = $this->query($query);
+        return $result->fetchAll();
+    }
+
+    protected function queryChecks($query) {
+        if (is_null($this->dbName)) {
+            throw new \Exception("No database selected");
+        }
+
+        if ($query instanceof Query\Query) {
+            $queryStr = $query->toString();
+        } else if (is_string($query)) {
+            $queryStr = $query;
+        } else {
+            throw new \Exception("Invalid query type");
+        }
+
+        return $queryStr;
+    }
+
+    public function getDatabaseName() {
+        return $this->dbName;
+    }
+
+    abstract protected function open();
+    abstract protected function close();
+    abstract public function rollback();
+    abstract public function commit();
+
+    abstract public function newQuery();
+}
