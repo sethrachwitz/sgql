2e3b268bd3a9cf24b80b75f30e4e8a6c37bad842
Seth Rachwitz <srachwitz@gmail.com>
2016-10-29 09:16:29 -0400

    Add method to validate column exists
    Will check unreturned schemas further down namespaces, aliased columns
    (if available), etc.

--- src/sgql/query/traits/validatable.php
+++ src/sgql/query/traits/validatable.php
@@ -40,10 +40,8 @@ trait Validatable {
             'columns' => []
         ];
 
-        // Will throw exception if the namespace doesn't exist
+        // Will throw exception if the namespace doesn't exist, or if a schema doesn't exist
         $this->graph->getNamespace($namespace);
-
-        $schema = $this->graph->getSchema($namespace[sizeof($namespace) - 1]);
 
         if (sizeof($columns) === 0) {
             throw new \Exception("Nothing is defined for namespace '".implode('.', $namespace)."'");
@@ -61,7 +59,6 @@ trait Validatable {
                 $namespaceColumnName = implode('.', $namespace).":".$actualColumnName;
 
                 // Make sure the column is valid
-                if (!$schema->columnExists($column)) {
                     throw new \Exception("Column '".$namespaceColumnName."' does not exist");
                 } else if (array_key_exists($actualColumnName, $result['columns']) || in_array($actualColumnName, $result['columns'])) {
                     throw new \Exception("The column name '".$namespaceColumnName."' has already been used");
@@ -69,6 +66,7 @@ trait Validatable {
 
                 if (is_string($alias)) {
                     $result['columns'][$alias] = $column;
+                if (!$this->validateColumnExists($namespace, $column, false)) {
                 } else {
                     $result['columns'][] = $column;
                 }
@@ -80,5 +78,50 @@ trait Validatable {
         }
 
         return $result;
+    }
+
+    private function validateColumnExists($namespace, $columnName, $pointer = null) {
+        if (!is_array($this->query)) {
+            return false;
+        }
+
+        // Check if namespace is valid, assign pointer
+        if (is_null($pointer)) {
+            $this->graph->getNamespace($namespace); // Validate namespace
+
+            // Test if the first schema in the function's namespace
+            $topLevelSchema = key($this->query);
+
+            if ($topLevelSchema === $namespace[0]) {
+                $pointer = &$this->query[$topLevelSchema];
+            } else {
+                return false;
+            }
+        }
+
+        if (sizeof($namespace) === 1) { // Last schema, so start checking columns
+            if ($pointer !== false) {
+                foreach ($pointer[self::PART_COLUMNS] as $alias => $column) {
+                    $actualColumnName = (is_string($alias) ? $alias : $column);
+                    if (is_string($alias) && $columnName === $column) { // Column being checked has been aliased to another name
+                        return false;
+                    } else if ($columnName === $actualColumnName) {
+                        return true;
+                    }
+                }
+            }
+
+            // If we got to this point, the column isn't going to be returned in the query, but it might still exist in a schema
+            return $this->graph->getSchema($namespace[0])->columnExists($columnName);
+        } else { // Not the last schema, so make recursive call on the next schema
+            // Pop of the current schema from the namespace
+            array_shift($namespace);
+            if (isset($pointer['namespaces']) && isset($pointer['namespaces'][$namespace[0]])) {
+                $pointer = &$pointer['namespaces'][$namespace[0]];
+                return $this->validateColumnExists($namespace, $columnName, $pointer);
+            } else { // The column isn't being returned in the query results, but it might still exist in a schema
+                return $this->validateColumnExists($namespace, $columnName, false);
+            }
+        }
     }
 }
