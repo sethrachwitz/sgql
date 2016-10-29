653f1becd8a5ff6f0c9b1e288531fc84fb7fbe97
Seth Rachwitz <srachwitz@gmail.com>
2016-10-29 09:13:24 -0400

    Transforms a parsed function into query array
    This turns the results of parsing a function into an array that can be
    used in the query class methods like select() as a column

--- src/sgql/query/traits/transformable.php
+++ src/sgql/query/traits/transformable.php
@@ -15,4 +15,30 @@ trait Transformable {
     private function transformSelect(array $parsed) {
         // Doesn't do anything yet, but this will transform a query string that was parsed into what $this->select needs
     }
+
+    protected function transformFunction(array $parsed) {
+        if ($parsed['type'] === Parser::TOKEN_LOCATION_AGGREGATION) {
+            $result['function'] = $parsed[Parser::TOKEN_AGGREGATION_FUNCTION_NAME]['value'];
+
+            foreach ($parsed[Parser::TOKEN_LOCATION][Parser::TOKEN_NAMESPACE] as $schema) {
+                $result['namespace'][] = $schema['value'];
+            }
+
+            $result['column'] = $parsed[Parser::TOKEN_LOCATION][Parser::TOKEN_ENTITY_NAME]['value'];
+        } else if ($parsed['type'] === Parser::TOKEN_NAMESPACE_COUNT) {
+            $result['function'] = $parsed[Parser::TOKEN_COUNT_FUNCTION_NAME]['value'];
+
+            foreach ($parsed[Parser::TOKEN_NAMESPACE] as $schema) {
+                $result['namespace'][] = $schema['value'];
+            }
+        }
+
+        if (isset($parsed[Parser::TOKEN_ALIAS])) {
+            return [
+                $parsed[Parser::TOKEN_ALIAS]['value'] => $result
+            ];
+        } else {
+            return [$result]; // Returned as array to follow the same format as returning a function with an alias
+        }
+    }
 }
