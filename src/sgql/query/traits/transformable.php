5216244e10b1a8df65029ed50fb446341569129b
Seth Rachwitz <srachwitz@gmail.com>
2017-01-08 22:29:22 -0500

    Converts a parsed function into a string

--- src/sgql/query/traits/transformable.php
+++ src/sgql/query/traits/transformable.php
@@ -16,6 +16,26 @@ trait Transformable {
         // Doesn't do anything yet, but this will transform a query string that was parsed into what $this->select needs
     }
 
+	protected function collapseFunction(array $parsed) {
+    	if ($parsed['type'] === Parser::TOKEN_LOCATION_AGGREGATION) {
+    		$alias = $parsed[Parser::TOKEN_ALIAS]['value'];
+    		$function = $parsed[Parser::TOKEN_AGGREGATION_FUNCTION_NAME]['value'].'(';
+    		foreach ($parsed[Parser::TOKEN_LOCATION][Parser::TOKEN_NAMESPACE] as $schema) {
+    			$function .= $schema['withBackticks'].'.';
+			}
+			$function = substr($function, 0, -1).':'.$parsed[Parser::TOKEN_LOCATION][Parser::TOKEN_ENTITY_NAME]['withBackticks'].')';
+    		return [$alias => $function];
+		} else if ($parsed['type'] === Parser::TOKEN_NAMESPACE_COUNT) {
+			$alias = $parsed[Parser::TOKEN_ALIAS]['value'];
+			$function = $parsed[Parser::TOKEN_COUNT_FUNCTION_NAME]['value'].'(';
+			foreach ($parsed[Parser::TOKEN_NAMESPACE] as $schema) {
+				$function .= $schema['withBackticks'].'.';
+			}
+			$function = substr($function, 0, -1).')';
+			return [$alias => $function];
+		}
+	}
+
     protected function transformFunction(array $parsed) {
         if ($parsed['type'] === Parser::TOKEN_LOCATION_AGGREGATION) {
             $result['function'] = $parsed[Parser::TOKEN_AGGREGATION_FUNCTION_NAME]['value'];
